3b8dbcdb94c9b2b83bf55e0aee04747763e4fb42
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-06-15 00:05:04 +0900

    Update config.py

--- cfg/config.py
+++ cfg/config.py
@@ -3,5 +3,17 @@
 '''
 
 EXTRACTOR_CONFIG = {
-    'chip': ''
+    'wifi_chip': 'bcm43455c0',  # Raspberry Pi B3+ / B4
+    'band': '2.4GHz',
+    'bandwidth': '20MHz'
+}
+
+PLOT_PARAMETER = {
+
+}
+
+
+NULL_PILOT_SUBCARRIER = {
+    '2.4GHz_20MHz': [],
+
 }
